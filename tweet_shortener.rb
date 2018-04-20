0197199d0b222e7f1d5783cfaff1f3c37a9a22b5
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-20 13:09:53 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,15 @@
 def dictionary
 
   dictionary = {
-    "hello" => "hi"
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" = "&"
   }
 
 end
